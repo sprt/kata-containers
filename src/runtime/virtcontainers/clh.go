8d13eb1d6ca920efcd5bd569cfdd4964e2c6ac8c
Dan Mihai <dmihai@microsoft.com>
2023-09-19 22:37:40 +0000

    runtime: use 10 PCI segments for the Guest (#59)
    There are 10 segments in the ACPI tables, and CLH works better when
    it uses all of them.
    
    Signed-off-by: Dan Mihai <dmihai@microsoft.com>

--- src/runtime/virtcontainers/clh.go
+++ src/runtime/virtcontainers/clh.go
@@ -465,6 +465,8 @@ func (clh *cloudHypervisor) enableProtection() error {
 			clh.vmconfig.Payload.SetHostData(snpZeroHostData)
 		}
 
+		clh.vmconfig.Platform.SetNumPciSegments(10)
+
 		return nil
 
 	default:
