9071f577497db8a114ee46512dccf36eadd78a90
Mitch Zhu <zhumc11@gmail.com>
2023-12-20 14:13:36 -0800

    Update package name

--- tools/osbuilder/rootfs-builder/cbl-mariner/config.sh
+++ tools/osbuilder/rootfs-builder/cbl-mariner/config.sh
@@ -6,6 +6,6 @@ OS_NAME=cbl-mariner
 OS_VERSION=${OS_VERSION:-2.0}
 LIBC="gnu"
 PACKAGES="kata-packages-uvm"
-[ "$CONF_GUEST" = yes ] && PACKAGES+=" kata-packages-coco-uvm"
+[ "$CONF_GUEST" = yes ] && PACKAGES+=" kata-packages-uvm-coco"
 [ "$AGENT_INIT" = no ] && PACKAGES+=" systemd"
 [ "$SECCOMP" = yes ] && PACKAGES+=" libseccomp"
