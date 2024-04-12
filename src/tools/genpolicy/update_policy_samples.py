42d6b0d8506f40cb96854bbab3a88541a46d626b
Saul Paredes <saulparedes@microsoft.com>
2024-04-11 23:57:07 -0700

    genpolicy: add yaml sample
    - Add yaml sample that uses v1 image manifest (rc-nginxhttps)
    - Fix static check (make check, genpolicy)
    - Update documentation
    
    Signed-off-by: Saul Paredes <saulparedes@microsoft.com>

--- src/tools/genpolicy/update_policy_samples.py
+++ src/tools/genpolicy/update_policy_samples.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import sys
 import json
-
+import time
 # runs genpolicy tools on the following files
 # should run this after any change to genpolicy
 # usage: python3 update_policy_samples.py
@@ -12,11 +12,10 @@ samples = ""
 with open('policy_samples.json') as f:
     samples = json.load(f)
 
-defaultYamls = samples["default"]
-
+default_yamls = samples["default"]
 silently_ignored = samples["silently_ignored"]
-
 no_policy = samples["no_policy"]
+needs_containerd_pull = samples["needs_containerd_pull"]
 
 file_base_path = "../../agent/samples/policy/yaml"
 
@@ -27,8 +26,14 @@ def runCmd(arg):
         print(f"`{arg}` failed with exit code {proc.returncode}. Stderr: {proc.stderr}, Stdout: {proc.stdout}")
     return proc
 
+def timeRunCmd(arg):
+    start = time.time()
+    runCmd(arg)
+    end = time.time()
+    print(f"Time taken: {round(end - start, 2)} seconds")
+
 # check we can access all files we are about to update
-for file in defaultYamls + silently_ignored + no_policy:
+for file in default_yamls + silently_ignored + no_policy:
     filepath = os.path.join(file_base_path, file)
     if not os.path.exists(filepath):
         print(f"filepath does not exists: {filepath}")
@@ -38,12 +43,15 @@ runCmd("cargo build")
 
 # update files
 genpolicy_path = "target/debug/genpolicy"
-for file in defaultYamls:
-    runCmd(f"{genpolicy_path} -y {os.path.join(file_base_path, file)}")
+
+total_start = time.time()
+
+for file in default_yamls + no_policy + needs_containerd_pull:
+    timeRunCmd(f"sudo {genpolicy_path} -d -y {os.path.join(file_base_path, file)}")
 
 for file in silently_ignored:
-    runCmd(f"{genpolicy_path} -y {os.path.join(file_base_path, file)} -s")
+    timeRunCmd(f"sudo {genpolicy_path} -d -s -y {os.path.join(file_base_path, file)}")
 
-for file in no_policy:
-    runCmd(f"{genpolicy_path} -y {os.path.join(file_base_path, file)}")
+total_end = time.time()
 
+print(f"Total time taken: {total_end - total_start} seconds")
